bb5c7adf89729ec3403f44af0cd70089b4c91e93
TanDC_VM <tandc_vm@novaon.dc>
2020-09-22 16:19:45 +0700

    Updates

---
+++ precache-manifest.028397329ea8bc48222817cc95480d30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a5bfc31cc54f1474f41148e9debd8a",
+    "url": "/beauty-point-dashboard/index.html"
+  },
+  {
+    "revision": "f771fdfe99493ae70d77",
+    "url": "/beauty-point-dashboard/static/css/main.ab4c12c7.chunk.css"
+  },
+  {
+    "revision": "3001a092926bb13b8e58",
+    "url": "/beauty-point-dashboard/static/js/2.2815f8ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beauty-point-dashboard/static/js/2.2815f8ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f771fdfe99493ae70d77",
+    "url": "/beauty-point-dashboard/static/js/main.df6189a1.chunk.js"
+  },
+  {
+    "revision": "02ea13ca70570d84313b",
+    "url": "/beauty-point-dashboard/static/js/runtime-main.e426ef21.js"
+  }
+]);
